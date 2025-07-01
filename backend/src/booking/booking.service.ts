5bf37ed04e5482a6a9f227674f207f4e23565f6a
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-07-01 12:01:49 +0300

    booking service modifications

--- backend/src/booking/booking.service.ts
+++ backend/src/booking/booking.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
