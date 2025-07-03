bbfbbd264987819b6fb69ddef78a137aca3ab2cc
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-07-03 10:40:23 +0300

    booking module modifications

--- backend/src/booking/booking.module.ts
+++ backend/src/booking/booking.module.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
